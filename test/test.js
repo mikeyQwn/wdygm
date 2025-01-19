086a7c9b7215b83a2c63e9a30a808f2f8ffe3d5d
mikeyQwn <mikeyQwn@gmail.com>
2025-01-19 03:53:08 +0300

    Add string, number and array validators, update validator methods, add `or` and `optonial` schema creation methods

--- test/test.js
+++ test/test.js
@@ -1,12 +1,18 @@
-import { isBoolean } from "../wdygm.js";
+import * as w from "../wdygm.js";
 
 class TF {
     #tcName;
+    /**
+     * @type {string=}
+     */
     #error;
 
+    /**
+     * @param {string} tcName
+     */
     constructor(tcName) {
         this.#tcName = tcName;
-        this.#error = null;
+        this.#error = undefined;
     }
 
     name() {
@@ -17,48 +23,67 @@ class TF {
         return this.#error;
     }
 
+    /**
+     * @param {boolean} v
+     * @param {string} [msg]
+     */
     assert(v, msg) {
         if (!!v) {
-            return true;
+            return;
         }
         this.#error = `assertion failed, expected: true, got ${v}`;
         if (msg) {
             this.#error += `. ${msg}`;
         }
-        return false;
+
+        throw null;
     }
 
+    /**
+     * @param {any} a
+     * @param {any} b
+     * @param {string} [msg]
+     */
     assertEq(a, b, msg) {
         if (a == b) {
-            return true;
+            return;
         }
         this.#error = `assertion failed, left = ${a}, right = ${b}`;
         if (msg) {
             this.#error += `. ${msg}`;
         }
-        return false;
+
+        throw null;
     }
 }
 
 function runTests() {
-    const tests = [testIsBoolean];
+    const tests = [testBoolean, testNumber, testString, testArray];
     for (let i = 0; i < tests.length; ++i) {
         const testFn = tests[i];
         const tf = new TF(testFn.name);
-        const isOk = testFn(tf);
-        if (isOk) {
+        try {
+            testFn(tf);
             console.log(`Test "${tf.name()}" passed`);
-        } else {
-            console.log(`Test "${tf.name()}" failed, error: ${tf.error()}`);
+        } catch (e) {
+            if (tf.error()) {
+                console.log(`Test "${tf.name()}" failed, error: ${tf.error()}`);
+            } else {
+                console.log(
+                    `Test "${tf.name()}" threw, unexpected error: ${e}`,
+                );
+            }
         }
     }
 }
+
+///////////// Tests begin here /////////////
 
 /**
  * @param {TF} tf
  * @return {boolean}
  */
-function testIsBoolean(tf) {
+function testBoolean(tf) {
     const cases = [
         [true, true],
         [false, true],
@@ -69,10 +94,92 @@ function testIsBoolean(tf) {
     ];
 
     for (const [input, expected] of cases) {
-        const got = isBoolean(input);
-        if (!tf.assertEq(got, expected, `Input = ${input}`)) {
-            return false;
-        }
+        const s = w.boolean();
+        const got = s.validate(input);
+        tf.assertEq(got, expected, `Input = ${input}`);
+    }
+
+    return true;
+}
+
+/**
+ * @param {TF} tf
+ * @return {boolean}
+ */
+function testNumber(tf) {
+    const cases = [
+        [true, false],
+        [0o123, true],
+        [0, true],
+        [1, true],
+        [-1, true],
+        [1.2, true],
+    ];
+
+    for (const [input, expected] of cases) {
+        const s = w.number();
+        const got = s.validate(input);
+        tf.assertEq(got, expected, `Input = ${input}`);
+    }
+
+    return true;
+}
+
+/**
+ * @param {TF} tf
+ * @return {boolean}
+ */
+function testString(tf) {
+    const cases = [
+        [true, false],
+        ["", true],
+        ["hello", true],
+        [{}, false],
+        ["true", true],
+        [Boolean(false), false],
+    ];
+
+    for (const [input, expected] of cases) {
+        const s = w.string();
+        const got = s.validate(input);
+        tf.assertEq(got, expected, `Input = ${input}`);
+    }
+
+    return true;
+}
+
+/**
+ * @param {TF} tf
+ * @return {boolean}
+ */
+function testArray(tf) {
+    /**
+     * @type [any, w.Schema<any>, boolean][]
+     */
+    const cases = [
+        [true, w.number(), false],
+        ["", w.number(), false],
+        ["hello", w.number(), false],
+        [{}, w.number(), false],
+        ["true", w.number(), false],
+        [Boolean(false), w.number(), false],
+        [[1, 2], w.number(), true],
+        [[], w.number(), true],
+        [["foo", "bar", 2], w.string(), false],
+        [["foo", "bar", "baz"], w.string(), true],
+        [[[], ["foo", "bar"], ["bar", "baz"]], w.array(w.string()), true],
+        [[[], ["foo", "bar"], [1, "baz"]], w.array(w.string()), false],
+        [
+            [[], ["foo", "bar"], [undefined, "baz"]],
+            w.array(w.string().optional()),
+            true,
+        ],
+    ];
+
+    for (const [input, schema, expected] of cases) {
+        const s = w.array(schema);
+        const got = s.validate(input);
+        tf.assertEq(got, expected, `Input = ${input}`);
     }
 
     return true;
--- wdygm.js
+++ wdygm.js
@@ -3,11 +3,127 @@
 // For more context, visit github.com/mikeyQwn/wdygm
 
 /**
- * @param {unknown} v
- * @return {v is boolean}
+ * @template T
  */
-export function isBoolean(v) {
-    return typeof v === "boolean";
+export class Schema {
+    /**
+     * @param {Schema<T>["validate"]} fn
+     */
+    constructor(fn) {
+        this.validate = fn;
+    }
+    /**
+     * @param {unknown} v
+     * @return {v is T}
+     */
+    validate(v) {
+        v;
+        return false;
+    }
+
+    /**
+     * @param {unknown} v
+     */
+    validate_throwing(v) {
+        if (!this.validate(v)) {
+            throw "validation error";
+        }
+    }
+
+    /**
+     * @template U
+     * @param {Schema<U>} other
+     * @returns {Schema<T | U>}
+     */
+    or(other) {
+        return new Schema(
+            /** @type {Schema<T | U>["validate"]} */
+            (v) => {
+                return this.validate(v) || other.validate(v);
+            },
+        );
+    }
+
+    /**
+     * @returns {Schema<T | undefined>}
+     */
+    optional() {
+        return this.or(undefined());
+    }
+}
+
+/**
+ * @template T
+ * @param {Schema<T>} s
+ * @returns {Schema<T[]>}
+ */
+export function array(s) {
+    /**
+     * @type {Schema<T[]>["validate"]}
+     */
+    const validator = (v) => {
+        if (!Array.isArray(v)) {
+            return false;
+        }
+
+        for (const el of v) {
+            if (!s.validate(el)) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    return new Schema(validator);
+}
+
+/**
+ * @returns {Schema<boolean>}
+ */
+export function boolean() {
+    return new Schema(
+        /** @type {Schema<boolean>["validate"]} */
+        (v) => {
+            return typeof v === "boolean";
+        },
+    );
+}
+
+/**
+ * @returns {Schema<number>}
+ */
+export function number() {
+    return new Schema(
+        /** @type {Schema<number>["validate"]} */
+        (v) => {
+            return typeof v === "number";
+        },
+    );
+}
+
+/**
+ * @returns {Schema<string>}
+ */
+export function string() {
+    return new Schema(
+        /** @type {Schema<string>["validate"]} */
+        (v) => {
+            return typeof v === "string";
+        },
+    );
+}
+
+/**
+ * @returns {Schema<undefined>}
+ */
+export function undefined() {
+    return new Schema(
+        /** @type {Schema<undefined>["validate"]} */
+        (v) => {
+            return typeof v === "undefined";
+        },
+    );
 }
 
 // Wdygm library source code ends here
